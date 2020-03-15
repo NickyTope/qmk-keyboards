ebb7101b4898fdfa7f9c455244bcf9136da5b154
Nicky Tope <nicky.tope@gmail.com>
2020-03-15 18:57:08 +1100

    update layout to match gergo style sym/num layers

--- labor/keymaps/default/keymap.c
+++ labor/keymaps/default/keymap.c
@@ -33,31 +33,73 @@ enum custom_keycodes {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+/* Base layer
+  *  CTBS = Control (hold), Backspace (tap)
+  *  SHENT = Shift (hold), Enter (tap)
+  *  SYMESC = Symbols (hold), Esc (tap)
+  *  NUMSPC = Numbers (hold), Space (tap)
+  * ,----------------------------------------.             ,----------------------------------------.
+  * | ESC |  Q   |  W   |  E   |  R   |  T   |             |   Y  |   U  |  I   |  O   |  P   | BSPC|
+  * |-----+------+------+------+------+------|             |------+------+------+------+------+-----|
+  * |CTBS |  A   |  S   |  D   |  F   |  G   |             |   H  |  J   |  K   |  L   |  ;   | ' " |
+  * |-----+------+------+------+------+------|             |------+------+------+------+------+-----|
+  * |SHFT |  Z   |  X   |  C   |  V   |  B   |             |   N  |  M   |  ,   |  .   |  / ? |SHENT|
+  * `-----+------+------+------+------+------'             `------+------+------+------+------+-----'
+  *             .------.   .------.   .------.  .----.     .------.   .------.   .------.
+  *             | WIN  |   |SYMESC|   |NUMSPC|  | TG |     |NUMSPC|   | ALT  |   | WIN  |
+  *             '------'   '------'   '------'  '----'     '------'   '------'   '------'
+  */
 [BASE] = LAYOUT_labor(
-                      KC_ESC,      KC_Q,      KC_W,  KC_E,   KC_R,   KC_T, KC_Y,    KC_U, KC_I, KC_O,   KC_P,    KC_BSPC,
-                      MT(MOD_LCTL, KC_BSPC),  KC_A,  KC_S,   KC_D,   KC_F, KC_G, KC_H,    KC_J, KC_K, KC_L,   KC_SCLN, KC_QUOT,
-                      KC_LSFT,     KC_Z,      KC_X,  KC_C,   KC_V,   KC_B, KC_N, KC_M,    KC_COMM, KC_DOT, KC_SLSH, MT(MOD_RSFT, KC_ENT),
-                      KC_LGUI, LT(SYMB, KC_ESC), LT(NUMB, KC_SPC),  TG_ENC,    KC_NO, LT(ARRW, KC_SPC), KC_RALT, KC_RGUI
+      KC_ESC,      KC_Q,      KC_W,  KC_E,   KC_R,   KC_T, KC_Y,    KC_U, KC_I, KC_O,   KC_P,    KC_BSPC,
+      MT(MOD_LCTL, KC_BSPC),  KC_A,  KC_S,   KC_D,   KC_F, KC_G, KC_H,    KC_J, KC_K, KC_L,   KC_SCLN, KC_QUOT,
+      KC_LSFT,     KC_Z,      KC_X,  KC_C,   KC_V,   KC_B, KC_N, KC_M,    KC_COMM, KC_DOT, KC_SLSH, MT(MOD_RSFT, KC_ENT),
+      KC_LGUI, LT(SYMB, KC_ESC), LT(NUMB, KC_SPC),  TG_ENC,    KC_NO, LT(NUMB, KC_SPC), KC_RALT, KC_RGUI
     ),
+/* Symbols layer
+ *
+ * ,----------------------------------------.             ,-----------------------------------------.
+ * |     |  !   |  @   |  {   |  }   |  |   |             |      |      |      |      |      |  \ | |
+ * |-----+------+------+------+------+------|             |------+------+------+------+------+------|
+ * |     |  #   |  $   |  (   |  )   |  `   |             |   +  |  -   |  /   |  *   |  %   |  ' " |
+ * |-----+------+------+------+------+------|             |------+------+------+------+------+------|
+ * |     |  %   |  ^   |  [   |  ]   |  ~   |             |   &  |  =   |  ,   |  .   |  / ? | - _  |
+ * `-----+------+------+------+------+------'             `------+------+------+------+------+------'
+ *             .------.   .------.   .------.  .----.     .------.   .------.
+ *             |      |   |      |   |      |  |    |     |      |   |      |
+ *             '------'   '------'   '------'  '----'     '------'   '------'
+ */
 [SYMB] = LAYOUT_labor(
-                      KC_GRV , KC_EXLM, KC_AT,   KC_HASH, KC_DLR, KC_PERC,  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSLS,
-                      _______, KC_GRV , _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_PIPE, KC_UNDS, KC_PLUS,
-                      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-                      _______, _______, KC_SCLN, _______,       _______, _______, _______, _______
+                      KC_GRV, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE,  _______,  _______, _______, _______, _______, KC_BSLS,
+                      _______, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,  KC_PLUS, KC_MINS, KC_SLSH, KC_ASTR, KC_PERC, KC_QUOT,
+                      _______, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD, KC_AMPR, KC_EQL,  KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
+                      _______, _______, _______, _______,       _______, _______, _______, _______
     ),
+/* Numbers layer
+ *
+ * ,----------------------------------------.             ,-----------------------------------------.
+ * |  ~  |  1   |  2   |  3   |  4   |  5   |             |   6  |  7   |  8   |  9   |  0   |  DEL |
+ * |-----+------+------+------+------+------|             |------+------+------+------+------+------|
+ * |     |  F1  |  F2  |  F3  |  F4  |  F5  |             | LEFT | DOWN |  UP  | RGHT | VOLD | VOLU |
+ * |-----+------+------+------+------+------|             |------+------+------+------+------+------|
+ * |     |  F7  |  F8  |  F9  |  F10 |  F11 |             |MLEFT |MDOWN | MUP  |MRGHT | PLAY | NEXT |
+ * `-----+------+------+------+------+------'             `------+------+------+------+------+------'
+ *             .------.   .------.   .------.  .----.     .------.   .------.
+ *             |      |   |      |   |      |  |    |     |      |   |      |
+ *             '------'   '------'   '------'  '----'     '------'   '------'
+ */
 [NUMB] = LAYOUT_labor(
-                      KC_TILD, KC_1, 	  KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-                      KC_LGUI, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, KC_LCBR, KC_RCBR, KC_BSLS, KC_MINS, KC_EQL, 
-                      _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______,
-                      _______, _______, _______, _______,  _______,  _______, _______, RESET
-    ),
+                      KC_TILD, KC_1, 	  KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
+                      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_VOLD, KC_VOLU,
+                      _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_MPLY, KC_MNXT,
+                      _______, _______, _______, _______,       _______, _______, _______, _______
+                      ),
 
-[ARRW] = LAYOUT_labor(
-                      _______, _______, _______, _______, _______, _______, _______, KC_HOME, _______, KC_END,  _______, _______,
-                      _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
-                      _______, _______, _______, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______,
-                      _______, _______, _______, _______,         _______, _______, _______, _______
-    )
+/* [ARRW] = LAYOUT_labor( */
+/*                       _______, _______, _______, _______, _______, _______, _______, KC_HOME, _______, KC_END,  _______, _______, */
+/*                       _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, */
+/*                       _______, _______, _______, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______, */
+/*                       _______, _______, _______, _______,         _______, _______, _______, _______ */
+/*     ) */
 };
 
 bool alt_enc = false;
