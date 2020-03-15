346ef054cb486535e6fdd38464cdfee261063c08
Nicky Tope <nicky.tope@gmail.com>
2020-03-15 17:52:55 +1100

    update keymap

--- labor/keymaps/default/keymap.c
+++ labor/keymaps/default/keymap.c
@@ -8,10 +8,12 @@
 
 #include QMK_KEYBOARD_H
 
-#define BASE 0 // default layer
-#define SYMB 1 // symbols
-#define NUMB 2 // numbers/motion
-#define ARRW 3 // numbers/motion
+enum layers {
+  BASE,
+  SYMB,
+  NUMB,
+  ARRW,
+};
 
 enum combos {
   LCOLON_ENTER,
@@ -26,62 +28,69 @@ combo_t key_combos[COMBO_COUNT] = {
   [DF_TAB] = COMBO(df_combo, KC_TAB)
 };
 
+enum custom_keycodes {
+                      TG_ENC = SAFE_RANGE,
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_labor(
-    LT(NUMB, KC_ESC),       KC_Q,  KC_W,   KC_E,   KC_R, KC_T, KC_Y,    KC_U, KC_I, KC_O,   KC_P,    KC_LBRC,
-    MT(MOD_LCTL, KC_BSPC),  KC_A,  KC_S,   KC_D,   KC_F, KC_G, KC_H,    KC_J, KC_K, KC_L,   KC_SCLN, KC_QUOT,
-    KC_RSFT,                KC_Z,  KC_X,   KC_C,   KC_V, KC_B, KC_N,    KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ENT,
-    MT(MOD_LGUI, KC_ENT), LT(SYMB, KC_SPC), LT(NUMB, KC_ESC), KC_NO,    KC_NO, LT(ARRW, KC_SPC), KC_RALT, KC_RGUI
+                      KC_ESC,      KC_Q,      KC_W,  KC_E,   KC_R,   KC_T, KC_Y,    KC_U, KC_I, KC_O,   KC_P,    KC_BSPC,
+                      MT(MOD_LCTL, KC_BSPC),  KC_A,  KC_S,   KC_D,   KC_F, KC_G, KC_H,    KC_J, KC_K, KC_L,   KC_SCLN, KC_QUOT,
+                      KC_LSFT,     KC_Z,      KC_X,  KC_C,   KC_V,   KC_B, KC_N, KC_M,    KC_COMM, KC_DOT, KC_SLSH, MT(MOD_RSFT, KC_ENT),
+                      KC_LGUI, LT(SYMB, KC_ESC), LT(NUMB, KC_SPC),  TG_ENC,    KC_NO, LT(ARRW, KC_SPC), KC_RALT, KC_RGUI
     ),
 [SYMB] = LAYOUT_labor(
-    KC_GRV , KC_EXLM, KC_AT,   KC_HASH, KC_DLR, KC_PERC,  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSLS,
-    KC_TRNS, KC_GRV , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_PIPE, KC_UNDS, KC_PLUS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                                        KC_TRNS, KC_TRNS, KC_SCLN, KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+                      KC_GRV , KC_EXLM, KC_AT,   KC_HASH, KC_DLR, KC_PERC,  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSLS,
+                      _______, KC_GRV , _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_PIPE, KC_UNDS, KC_PLUS,
+                      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                      _______, _______, KC_SCLN, _______,       _______, _______, _______, _______
     ),
 [NUMB] = LAYOUT_labor(
-    KC_TILD, KC_1, 	  KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS,
-    KC_LGUI, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS, KC_LCBR, KC_RCBR, KC_BSLS, KC_MINS, KC_EQL, 
-    KC_TRNS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_TRNS, KC_TRNS,
-                                        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,         KC_TRNS,  KC_TRNS, KC_TRNS, RESET
+                      KC_TILD, KC_1, 	  KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+                      KC_LGUI, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, KC_LCBR, KC_RCBR, KC_BSLS, KC_MINS, KC_EQL, 
+                      _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______,
+                      _______, _______, _______, _______,  _______,  _______, _______, RESET
     ),
 
 [ARRW] = LAYOUT_labor(
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_TRNS, KC_END,  KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_TRNS, KC_TRNS,
-                                        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+                      _______, _______, _______, _______, _______, _______, _______, KC_HOME, _______, KC_END,  _______, _______,
+                      _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
+                      _______, _______, _______, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______,
+                      _______, _______, _______, _______,         _______, _______, _______, _______
     )
 };
 
+bool alt_enc = false;
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+  case TG_ENC:
+    if (record->event.pressed) {
+      // when keycode QMKBEST is pressed
+      alt_enc = !alt_enc;
+    } else {
+      // when keycode QMKBEST is released
+    }
+    break;
+  }
+  return true;
+};
+
 void encoder_update_user(uint8_t index, bool clockwise) {
+  // 3 ifs.. surely we can do better than this?
   if (index == 0) {
     if (clockwise) {
-      SEND_STRING("seb");
-      // switch (layer_state) {
-      //   case 2:
-      //     tap_code(KC_DOWN);
-      //     break;
-      //   case 1:
-      //     SEND_STRING(SS_LCTRL("e"));
-      //     break;
-      //   case 0:
-      //     SEND_STRING(SS_LCTRL("u"));
-      //     break;
-      // }
+      if (alt_enc) {
+        tap_code(KC_PGDN);
+      } else {
+        tap_code(KC_DOWN);
+      }
     } else {
-      SEND_STRING("dad");
-      // switch (layer_state) {
-      //   case 2:
-      //     tap_code(KC_UP);
-      //     break;
-      //   case 1:
-      //     SEND_STRING(SS_LCTRL("y"));
-      //     break;
-      //   case 0:
-      //     SEND_STRING(SS_LCTRL("d"));
-      //     break;
-      // }
+      if (alt_enc) {
+        tap_code(KC_PGUP);
+      } else {
+        tap_code(KC_UP);
+      }
     }
   }
 }
